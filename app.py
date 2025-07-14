7d8aeaff4114bf6c89a2dd331ad734d24d61d668
Shidosu <66335264+Shidosu@users.noreply.github.com>
2025-07-14 04:10:10 +0000

    edited .py file

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ def index():
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-    return(render_template("prediction.html", r=(-50.6*q)+90.2))
 
     #load model
     model = joblib.load('dbs.jl')
     
     #predict model
     p = model.predict([[q]])
+    # return(render_template("prediction.html", r=(-50.6*q)+90.2))
+    return(render_template("prediction.html", r=p))
 
 if __name__ == "__main__":
     app.run()
